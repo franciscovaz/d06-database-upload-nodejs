63e8d796484587872f22b370ee224f0b74c90c80
Francisco Vaz <ftomasvaz@gmail.com>
2020-04-19 22:37:29 +0100

    Get, post, delete transaction Get balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,8 +11,52 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const balance = transactions.reduce(
+      (amount, { type, value }) => ({
+        ...amount,
+        [type]: Number(amount[type]) + Number(value),
+      }),
+      { income: 0, outcome: 0 },
+    );
+
+    const { income, outcome } = balance;
+
+    return {
+      ...balance,
+      total: income - outcome,
+    };
   }
 }
 
 export default TransactionsRepository;
+
+// outra forma de fazer o getBalance
+/*
+const balance = transactions.reduce(
+  (accumulator, transaction) => {
+    switch (transaction.type) {
+      case 'income':
+        accumulator.income += Number(transaction.value);
+        break;
+
+      case 'outcome':
+        accumulator.outcome += Number(transaction.value);
+        break;
+
+      default:
+        break;
+    }
+
+    return accumulator;
+  },
+  {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  },
+);
+const total = income - outcome;
+
+return { income, outcome, total}
+*/
